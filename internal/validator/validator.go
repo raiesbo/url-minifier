197b5d4be92aad918fa7445979d5d41c2c661447
raiesbo <raiesbo@gmail.com>
2025-12-06 23:47:01 +0100

    remove parentheses from function comments for consistency

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -28,12 +28,12 @@ func (v *Validator) AddFieldError(field string, message string) {
 	}
 }
 
-// NotBlank() returns true if a value is not an empty string.
+// NotBlank returns true if a value is not an empty string.
 func NotBlank(value string) bool {
 	return strings.TrimSpace(value) != ""
 }
 
-// ValidURL() returns true if the value has a valid URL format.
+// ValidURL returns true if the value has a valid URL format.
 func ValidURL(value string) bool {
 	_, err := url.Parse(value)
 	return err == nil
